e6c9ae70054d1ce5e48afae225f5dc58579daf07
kuliinch <kuliinch@gmail.com>
2025-07-10 23:10:45 +0200

    Forgot to push recent changes

--- lexer.c
+++ lexer.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <strings.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "lexer.h"
+#include "tokens.h"
+#include "moods.h"
 
 // INT_MAX = 2147483647
 
@@ -20,6 +23,49 @@ Lexer load(FILE *file){
 	return l;
 }
 
+void eval(const char* unproc) {
+	// TODO Tokenize and add token to array (TODO where?)
+	printf("[+] [DEBUG] Eval has been called\n");
+}
+
+int typecheck(char* chr, int* m){
+	char ch = *chr;
+	//Use ctype.h 
+	
+	if(m == NULL){
+		if(isalpha(ch)) return LETTER;
+		else if(isdigit(ch)) return NUMBER;
+		else if(ispunct(ch)) return SYMBOL;
+		else if(isspace(ch)) return SPACE;
+		return NOTHING;
+	}
+	int pmo = *m;
+	if(pmo==OD) return OD;
+	else if (pmo==OS) return OS;
+	// TODO If the character is an escape character ('\') do something different
+	if(ch=='\\'){
+		if(pmo==OD) return ODE;
+		else if(pmo==ODE) return ODE2;
+		else if(pmo==ODE2) return ODE;
+		else if(pmo==OS) return OSE;
+		else if(pmo==OSE) return OSE2;
+		else return 999;
+	}
+	else if(ch=='"'){
+		if(pmo==OD) return CD;
+		else if(pmo==ODE) return ODE2;
+		else if(pmo==ODE2) return CD; 
+		else return OD;
+	}
+	else if(ch=='\''){
+		if(pmo==OS) return CS;
+		else if(pmo==OSE) return OSE2;
+		else if(pmo==OSE2) return CS; 
+		else return OS;
+	}
+	return SYMBOL;
+}	
+
 void lex(const char* fn){ // TODO return token array pointer and size 
 	FILE *file = fopen(fn,"r");
 	Lexer lex = load(file); 
@@ -29,16 +75,43 @@ void lex(const char* fn){ // TODO return token array pointer and size
 	}
 	else{
 		printf("[+] [DEBUG] Lexer's buff is: %p\n",lex.buff);
+		char curr = lex.buff[0];
+		char* currp = &curr;
+		int mood = typecheck(currp, NULL); 
+		int* moodp = &mood;
+			// Mood keeps track of the previous char that has been shown
+			// 0 = nothing, 1 = letter, 2 = number, 3 = symbol, 4 = space/nl
+			// TODO add more types for escape sequences, literals, comments, etc.
+		int type = 0;
+		char last[100]; // WARNING Unprocessed token max size 
+		int lastp = 0;
 		do{
-		int mode = 0; // Undefined mode
-					  // 1 charmode, 2 symmode, 3 spacemode
-		printf("[+] [DEBUG] lex.pos = %d\n",lex.pos);
-		printf("[+] [DEBUG] Current char: %c\n",lex.buff[lex.pos]);
-		
-		//Lexing, assinging ints (with enum) as we go along
+			curr = lex.buff[lex.pos];
+			printf("[+] [DEBUG] lex.pos = %d\n",lex.pos);
+			printf("[+] [DEBUG] Current char: %c\n",curr);
+			
+			//Lexing, assinging ints (with enum) as we go along
+			/* If the character is of type X and mood has not changed, continue
+			 * Once the mood has changed, evaluate the last token*/		
+			type = typecheck(currp,moodp);
 
-		lex.pos++;
-		} while (lex.pos < lex.bufflen-1); //Doesn't read the terminator
-										   //Perhaps it should to make tokenizing easier?
+			printf("[+] [DEBUG] Curr type: %d\n",type);
+
+			if(type==mood){
+				last[lastp] = curr;
+				lastp++;
+			}
+			else{
+				printf("[+] [DEBUG] Mood change!, last = %s\n",last);
+				// Evaluate and tokenize, then empty last
+				last[lastp] = '\0';
+				eval(last);
+				last[0] = curr;
+				last[1] = '\0';
+				lastp = 1;
+			}
+
+			lex.pos++;
+		} while(lex.pos < lex.bufflen); 
 	}
 }
